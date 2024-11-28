2539098007f90766c6f9845a7c5095ba662ab3e0
Danny Azimi <k6m3lo@gmail.com>
2024-11-27 20:17:14 -0800

    Added the generate_password() function for the random password

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 def save():
     entered_website = website_text.get()
@@ -19,6 +20,26 @@ def save():
                 email_text.delete(0,END)
                 email_text.insert(0,"@gmail.com")
                 password_text.delete(0,END)
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [random.choice(letters) for n in range(random.randint(8, 10))]
+    password_symbols = [random.choice(symbols) for n in range(random.randint(2, 4))]
+    password_numbers = [random.choice(numbers) for n in range(random.randint(2, 4))]
+    password_list = password_letters + password_symbols + password_numbers
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+
+    password_text.delete(0,END)
+    password_text.insert(0,password)
 
 window = Tk()
 window.title("Password Manager")
@@ -46,7 +67,7 @@ email_label.grid(row=2,column=0, sticky=EW)
 #generate button and password text entry
 password_text = Entry(width=21)
 password_text.grid(row=3,column=1, sticky=EW)
-generate_button = Button(text = "Generate",width= 9)
+generate_button = Button(text = "Generate",width= 9,command=generate_password)
 generate_button.grid(column = 2, row=3, sticky=EW)
 password_label = Label(text = "Password: ")
 password_label.grid(row=3, column = 0, sticky=EW)
